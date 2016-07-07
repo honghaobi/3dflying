ec6d4f0732e6164cd53347af51a9b127ab0b787d
henrybi <henrybi@u.washington.edu>
2016-07-06 17:50:13 -0700

    lights initialized

--- js/game.js
+++ js/game.js
@@ -33,7 +33,7 @@ function init(event){
   createScene();
   createLights();
   createPlane();
-  createSea();
+  createLand();
   createSky();
   loop();
 }
@@ -80,4 +80,29 @@ function createScene() {
 
   window.addEventListener('resize', handleWindowResize, false);
 
+}
+
+//Create Lights
+
+var ambientLight,
+    hemisphereLight,
+    shadowLight;
+
+function createLights() {
+
+  hemisphereLight = new THREE.HemisphereLight(0xaaaaaa,0x000000, .9)
+  shadowLight = new THREE.DirectionalLight(0xffffff, .9);
+  shadowLight.position.set(250, 350, 350);
+  shadowLight.castShadow = true;
+  shadowLight.shadow.camera.left = -400;
+  shadowLight.shadow.camera.right = 400;
+  shadowLight.shadow.camera.top = 400;
+  shadowLight.shadow.camera.bottom = -400;
+  shadowLight.shadow.camera.near = 1;
+  shadowLight.shadow.camera.far = 1000;
+  shadowLight.shadow.mapSize.width = 2048;
+  shadowLight.shadow.mapSize.height = 2048;
+
+  scene.add(hemisphereLight);
+  scene.add(shadowLight);
 }
