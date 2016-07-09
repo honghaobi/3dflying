dfcc8495789cb75b1c7fbdf636f67d7c79ac46d2
henrybi <henrybi@u.washington.edu>
2016-07-08 21:27:08 -0700

    moving astroid works

--- js/game.js
+++ js/game.js
@@ -28,7 +28,78 @@ var HEIGHT,
 
 
 //For animation frames
+
 var mixers = [];
+
+//Game Setting
+
+var deltaTime = 0;
+var newTime = new Date().getTime();
+var oldTime = new Date().getTime();
+var birdsPool = [];
+var fieldDistance, energyBar, replayMessage, fieldLevel, levelCircle;
+
+var game = {speed:0,
+        initSpeed:.00035,
+        baseSpeed:.00035,
+        targetBaseSpeed:.00035,
+        incrementSpeedByTime:.0000025,
+        incrementSpeedByLevel:.000005,
+        distanceForSpeedUpdate:100,
+        speedLastUpdate:0,
+
+        distance:0,
+        ratioSpeedDistance:50,
+        energy:100,
+        ratioSpeedEnergy:3,
+
+        level:1,
+        levelLastUpdate:0,
+        distanceForLevelUpdate:1000,
+
+        planeDefaultHeight:100,
+        planeAmpHeight:80,
+        planeAmpWidth:75,
+        planeMoveSensivity:0.005,
+        planeRotXSensivity:0.0008,
+        planeRotZSensivity:0.0004,
+        planeFallSpeed:.001,
+        planeMinSpeed:1.2,
+        planeMaxSpeed:1.6,
+        planeSpeed:0,
+        planeCollisionDisplacementX:0,
+        planeCollisionSpeedX:0,
+
+        planeCollisionDisplacementY:0,
+        planeCollisionSpeedY:0,
+
+        seaRadius:600,
+        seaLength:800,
+        //seaRotationSpeed:0.006,
+        wavesMinAmp : 5,
+        wavesMaxAmp : 20,
+        wavesMinSpeed : 0.001,
+        wavesMaxSpeed : 0.003,
+
+        cameraFarPos:500,
+        cameraNearPos:150,
+        cameraSensivity:0.002,
+
+        coinDistanceTolerance:15,
+        coinValue:3,
+        coinsSpeed:.5,
+        coinLastSpawn:0,
+        distanceForCoinsSpawn:100,
+
+        ennemyDistanceTolerance:10,
+        ennemyValue:10,
+        ennemiesSpeed:.6,
+        ennemyLastSpawn:0,
+        distanceForEnnemiesSpawn:50,
+
+        status : "playing",
+
+}
 
 
 //Initializing
@@ -195,29 +266,47 @@ Clouds = function(){
   }
 }
 
-var birdsPool = [];
-
 Bird = function(){
+  // var self = this;
+  // var flamingoLoader = new THREE.JSONLoader();
+  //
+  // flamingoLoader.load( "models/flamingo.js", function( geometry ) {
+  //   var material = new THREE.MeshPhongMaterial({
+  //     color: 0xffffff,
+  //     specular: 0xffffff,
+  //     shininess: 20,
+  //     morphTargets: true,
+  //     vertexColors: THREE.FaceColors,
+  //     shading: THREE.FlatShading
+  //   });
+  //   console.log('this', self);
+	// 	self.mesh = new THREE.Mesh( geometry, material );
+	// 	// var s = 0.35;
+  //   var s = 35;
+	// 	self.mesh.scale.set( s, s, s );
+	// 	self.mesh.position.y = 200;
+	// 	self.mesh.rotation.y = -1;
+	// 	self.mesh.castShadow = true;
+	// 	self.mesh.receiveShadow = true;
+  //   self.angle = 0;
+  //   self.dist = 0;
+	// 	// scene.add( this.mesh );
+	// 	var mixer = new THREE.AnimationMixer( this.mesh );
+	// 	mixer.clipAction( geometry.animations[ 0 ] ).setDuration( 1 ).play();
+	// 	mixers.push( mixer );
+  // });
 
-  var flamingoLoader = new THREE.JSONLoader();
-
-  flamingoLoader.load( "models/flamingo.js", function( geometry ) {
-    var material = new THREE.MeshPhongMaterial( { color: 0xffffff, specular: 0xffffff, shininess: 20, morphTargets: true, vertexColors: THREE.FaceColors, shading: THREE.FlatShading } );
-
-		var mesh = new THREE.Mesh( geometry, material );
-		var s = 0.35;
-		mesh.scale.set( s, s, s );
-		mesh.position.y = 200;
-		mesh.rotation.y = -1;
-		mesh.castShadow = true;
-		mesh.receiveShadow = true;
-		scene.add( mesh );
-		var mixer = new THREE.AnimationMixer( mesh );
-		mixer.clipAction( geometry.animations[ 0 ] ).setDuration( 1 ).play();
-		mixers.push( mixer );
-
-
+  var geom = new THREE.TetrahedronGeometry(30,2);
+  var mat = new THREE.MeshPhongMaterial({
+    color:colors.navy,
+    shininess:0,
+    specular:0xffffff,
+    shading:THREE.FlatShading
   });
+  this.mesh = new THREE.Mesh(geom,mat);
+  this.mesh.castShadow = true;
+  this.angle = 0;
+  this.dist = 0;
 
 }
 
@@ -230,19 +319,62 @@ var counter = 0;
 
 BirdsHolder.prototype.spawnBirds = function(){
 
+    var nBirds = game.level;
 
-    counter +=1;
+    for (var i=0; i<nBirds; i++){
+      var bird;
+      if (birdsPool.length) {
+        console.log('birdspool poping');
+        bird = birdsPool.pop();
+      } else {
+        bird = new Bird();
+      }
 
-
-    if (counter < 2) {
-      bird = new Bird();
-    } else {
-      return;
-    }
+    bird.angle = - (i*0.1);
+    bird.distance = game.seaRadius + game.planeDefaultHeight + (-1 + Math.random() * 2) * (game.planeAmpHeight-20);
+    console.log('bird distance', bird.distance);
+    bird.mesh.position.y = -game.seaRadius + Math.sin(bird.angle)*bird.distance;
+    console.log('bird mesh pos y', bird.mesh.position.y);
+    bird.mesh.position.x = Math.cos(bird.angle)*bird.distance;
+    console.log('bird mesh pos x', bird.mesh.position.x);
 
     this.mesh.add(bird.mesh);
     this.birdsInUse.push(bird);
+  }
+}
 
+BirdsHolder.prototype.rotateBirds = function(){
+  for (var i=0; i<this.birdsInUse.length; i++){
+    var bird = this.birdsInUse[i];
+    bird.angle += game.speed*deltaTime*game.ennemiesSpeed;
+
+    if (bird.angle > Math.PI*2) bird.angle -= Math.PI*2;
+
+    bird.mesh.position.y = -game.seaRadius + Math.sin(bird.angle)*bird.distance;
+    bird.mesh.position.x = Math.cos(bird.angle)*bird.distance;
+    bird.mesh.rotation.z += Math.random()*.1;
+    bird.mesh.rotation.y += Math.random()*.1;
+
+    //var globalEnnemyPosition =  bird.mesh.localToWorld(new THREE.Vector3());
+    var diffPos = house.scene.position.clone().sub(bird.mesh.position.clone());
+    var d = diffPos.length();
+    if (d<game.ennemyDistanceTolerance){
+      particlesHolder.spawnParticles(bird.mesh.position.clone(), 15, Colors.red, 3);
+
+      birdsPool.unshift(this.birdsInUse.splice(i,1)[0]);
+      this.mesh.remove(bird.mesh);
+      game.planeCollisionSpeedX = 100 * diffPos.x / d;
+      game.planeCollisionSpeedY = 100 * diffPos.y / d;
+      ambientLight.intensity = 2;
+
+      removeEnergy();
+      i--;
+    }else if (bird.angle > Math.PI){
+      birdsPool.unshift(this.birdsInUse.splice(i,1)[0]);
+      this.mesh.remove(bird.mesh);
+      i--;
+    }
+  }
 }
 
 // 3D Models
@@ -258,6 +390,7 @@ function createHouse(){
 
     houseCollada.scene.rotation.y = 0;
     houseCollada.scene.rotation.x = -1.6;
+
 
     var housePartsArray = houseCollada.scene.children[0].children[1].children;
     for (var i = 0; i < housePartsArray.length; i++) {
@@ -283,12 +416,11 @@ function createSky(){
 }
 
 function createBirds(){
-  for (var i=0; i<2; i++){
+  for (var i=0; i<10; i++){
     var bird = new Bird();
     birdsPool.push(bird);
   }
   birdsHolder = new BirdsHolder();
-  //ennemiesHolder.mesh.position.y = -game.seaRadius;
   scene.add(birdsHolder.mesh)
 }
 
@@ -301,17 +433,67 @@ function renderAnimatedModels() {
   }
 }
 
+var count = 0;
+
 function loop(){
 
-  renderAnimatedModels();
+  //Game Logic
 
-  birdsHolder.spawnBirds();
+  newTime = new Date().getTime();
+  deltaTime = newTime-oldTime;
+  oldTime = newTime;
+
+  if (game.status=="playing"){
+
+    if (Math.floor(game.distance)%game.distanceForSpeedUpdate == 0 && Math.floor(game.distance) > game.speedLastUpdate){
+      game.speedLastUpdate = Math.floor(game.distance);
+      game.targetBaseSpeed += game.incrementSpeedByTime*deltaTime;
+    }
+
+
+    if (Math.floor(game.distance)%game.distanceForEnnemiesSpawn == 0 && Math.floor(game.distance) > game.ennemyLastSpawn){
+      game.ennemyLastSpawn = Math.floor(game.distance);
+      console.log('spawn', count);
+      count ++;
+      birdsHolder.spawnBirds();
+    }
+
+    if (Math.floor(game.distance)%game.distanceForLevelUpdate == 0 && Math.floor(game.distance) > game.levelLastUpdate){
+      game.levelLastUpdate = Math.floor(game.distance);
+      game.level++;
+      // fieldLevel.innerHTML = Math.floor(game.level);
+      // console.log('game level', game.level);
+      game.targetBaseSpeed = game.initSpeed + game.incrementSpeedByLevel*game.level
+    }
+
   updateHouse();
+  updateDistance();
+  game.baseSpeed += (game.targetBaseSpeed - game.baseSpeed) * deltaTime * 0.02;
+  game.speed = game.baseSpeed * game.planeSpeed;
+
+} else if(game.status=="gameover") {
+
+}
+
+  renderAnimatedModels();
   updateCameraFov();
+  birdsHolder.rotateBirds();
+
   ground.mesh.rotation.z += .005;
   sky.mesh.rotation.z += .005;
+
+
   renderer.render(scene, camera);
   requestAnimationFrame(loop);
+}
+
+function updateDistance(){
+  game.distance += game.speed*deltaTime*game.ratioSpeedDistance;
+  // fieldDistance.innerHTML = Math.floor(game.distance);
+  // console.log('distance', game.distance);
+  var d = 502*(1-(game.distance%game.distanceForLevelUpdate)/game.distanceForLevelUpdate);
+  // levelCircle.setAttribute("stroke-dashoffset", d);
+
 }
 
 function updateHouse(){
@@ -348,6 +530,11 @@ function updateHouse(){
     houseRot.z = -(tx-housePos.x) * xRotSpeed;
 
   }
+
+  game.planeSpeed = normalize(mousePos.x,-.5,.5,game.planeMinSpeed, game.planeMaxSpeed);
+
+
+
 }
 
 function updateCameraFov(){
